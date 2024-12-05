ced264284ba636d212c0036e376e61fd9d0c88e2
IceYetiWins <101294194+IceYetiWins@users.noreply.github.com>
2024-12-05 15:57:49 -0500

    Begin creating actual auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoTesting.java
@@ -23,16 +23,23 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 public class AutoTesting extends LinearOpMode {
     @Override
     public void runOpMode(){
-        Pose2d initialPose = new Pose2d(0, 0, Math.PI/2); //need to add correct pose
+        Pose2d initialPose = new Pose2d(56.5, 8.25, Math.PI/2); //need to add correct pose
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         //should move diagonal right while turning to face right, then move backwards, then go back to starting spot
-        TrajectoryActionBuilder tab = drive.actionBuilder(initialPose)
+        TrajectoryActionBuilder exampleTab = drive.actionBuilder(initialPose)
                 .splineTo(new Vector2d(48, 48), 0)
                 .waitSeconds(2)
                 .lineToX(0)
                 .waitSeconds(3)
+                .setTangent(5 * Math.PI/2)
                 .splineTo(new Vector2d(0, 0), Math.PI/2);
+
+        TrajectoryActionBuilder tab = drive.actionBuilder(initialPose)
+                .splineToConstantHeading(new Vector2d(22, 24), Math.PI / 2)
+                //grab first piece
+                .splineTo(new Vector2d(22, 24), 5 * Math.PI / 2); //rotate to 5pi/2
+                //drop off piece
 
         Actions.runBlocking(new SequentialAction(tab.build())); //runs trajectory as action
     }
