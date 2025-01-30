f033ab51c83dd83bddeccc7f6020136019a4c3cc
Ice Yeti <101294194+IceYetiWins@users.noreply.github.com>
2025-01-30 16:27:45 -0500

    added arm rotate test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmRotateTesting.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ArmRotateTesting extends OpMode {
+    private MotorEx rotateMotor;
+    private static final double ROTATE_SPEED = .5;
+
+    @Override
+    public void init(){
+        rotateMotor = new MotorEx(hardwareMap, "rotateMotor", Motor.GoBILDA.RPM_312);
+        rotateMotor.setRunMode(Motor.RunMode.RawPower);
+    }
+
+    @Override
+    public void loop(){
+        if (gamepad1.a){
+            rotateMotor.set(ROTATE_SPEED);
+        } else if (gamepad1.b) {
+            rotateMotor.set(-ROTATE_SPEED);
+        } else {
+            rotateMotor.set(0);
+        }
+    }
+}
