a7b642656a2b70497e713e48a9471c513bddf0e4
Ice Yeti <101294194+IceYetiWins@users.noreply.github.com>
2025-02-15 20:01:45 -0500

    rename variables in ClawCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ClawCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ClawCommand.java
@@ -8,26 +8,26 @@ import java.util.function.BooleanSupplier;
 
 public class ClawCommand extends CommandBase {
     private final ClawSubsystem clawSubsystem;
-    private final BooleanSupplier close;
-    private final double openDegrees1, closedDegrees1, openDegrees2, closedDegrees2;
+    private final BooleanSupplier open;
+    private final double closedDegrees1, openDegrees1, closedDegrees2, openDegrees2;
 
-    public ClawCommand(ClawSubsystem subsystem, BooleanSupplier close, double openDegrees1, double closedDegrees1, double openDegrees2, double closedDegrees2){
+    public ClawCommand(ClawSubsystem subsystem, BooleanSupplier open, double closedDegrees1, double openDegrees1, double closedDegrees2, double openDegrees2){
         clawSubsystem = subsystem;
 
-        this.close = close;
-        this.openDegrees1 = openDegrees1;
-        this.openDegrees2 = openDegrees2;
+        this.open = open;
         this.closedDegrees1 = closedDegrees1;
         this.closedDegrees2 = closedDegrees2;
+        this.openDegrees1 = openDegrees1;
+        this.openDegrees2 = openDegrees2;
         addRequirements(subsystem);
     }
 
     @Override
     public void execute(){
-        if (close.getAsBoolean()){
-            clawSubsystem.toAngle(closedDegrees1, closedDegrees2);
-        } else {
+        if (open.getAsBoolean()){
             clawSubsystem.toAngle(openDegrees1, openDegrees2);
+        } else {
+            clawSubsystem.toAngle(closedDegrees1, closedDegrees2);
         }
     }
 }
